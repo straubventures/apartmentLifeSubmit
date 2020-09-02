4a9a8b871169c676fda1f2f359e02adcbddb5905
straub.ventures@gmail.com <straubfamily@Nathans-MacBook-Pro.local>
2020-09-02 12:22:12 -0700

    added port

---
+++ clrForm.js
@@ -0,0 +1,6 @@
+function clrForm() {
+    document.getElementById('email').value = ''
+    document.getElementById('firstName').value = ''
+    document.getElementById('lastName').value = '';
+    return
+};
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function clrForm() {
-    document.getElementById('email').value = ''
-    document.getElementById('firstName').value = ''
-    document.getElementById('lastName').value = '';
-    return
-};
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function () {
+    console.log('Chat server running');
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        io.emit('message', msg);
+    });
+});
