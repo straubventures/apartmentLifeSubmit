907f02d20cdcc1184a1faba8ed6746f49b90b48c
straub.ventures@gmail.com <straubfamily@Nathans-MacBook-Pro.local>
2020-09-03 12:37:40 -0700

    deploy with safari compatability

--- sendForm.js
+++ sendForm.js
@@ -1,12 +1,23 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxMKJ8Uh3dbmyXlS1TpbduSCzkQFKRrL9leuEeooJyARNI3tuM9/exec'
+const scriptURL2 = 'https://script.google.com/macros/s/AKfycbzoTcWYv-KHTkJ1Y-mhjXNSI7K_dJCm8drDe3DBUCQ1RgZU_1VN/exec'
 // Google Apps Script for sending documents to Google Sheets
 const form = document.forms['submit-to-google-sheet']
 
 $('#form').on("submit", e => {
-    // e.preventDefault()
+    e.preventDefault()
     // BUG FOUND
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    console.log("working")
+    fetch(scriptURL, { method: 'POST', body: new FormData(form), mode: 'no-cors' })
         .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message))
+        .catch(error => console.error('Error!', error.message));
+    fetch(scriptURL2, { method: 'POST', body: new FormData(form), mode: 'no-cors' })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+
+
+    // form.action = 'https://script.google.com/macros/s/AKfycbxMKJ8Uh3dbmyXlS1TpbduSCzkQFKRrL9leuEeooJyARNI3tuM9/exec';
+    // form.method = "POST"
+    // form.action = 'https://script.google.com/macros/s/AKfycbzoTcWYv-KHTkJ1Y-mhjXNSI7K_dJCm8drDe3DBUCQ1RgZU_1VN/exec';
+
 
 })
--- uploadFile.js
+++ uploadFile.js
@@ -4,11 +4,17 @@ $('#uploadfile').on("change", function () {
     var fr = new FileReader();
     fr.fileName = file.name
     fr.onload = function (e) {
+
         e.target.result
+        e.preventDefault();
         html = '<input type="hidden" name="data" value="' + e.target.result.replace(/^.*,/, '') + '" >';
         html += '<input type="hidden" name="mimetype" value="' + e.target.result.match(/^.*(?=;)/)[0] + '" >';
         html += '<input type="hidden" name="filename" value="' + e.target.fileName + '" >';
         $("#data").empty().append(html);
+        console.log(html)
+        document.getElementById('form').action = 'https://script.google.com/macros/s/AKfycbzoTcWYv-KHTkJ1Y-mhjXNSI7K_dJCm8drDe3DBUCQ1RgZU_1VN/exec'
+
     }
     fr.readAsDataURL(file);
+
 });
